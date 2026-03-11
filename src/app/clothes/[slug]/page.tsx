741d76f1255a7aff6863d4cd0f5edb76964bf264
Stask0u <stanislav.ivanov136@gmail.com>
2026-03-11 11:06:05 +0200

    slug changes

--- src/app/clothes/[slug]/page.tsx
+++ src/app/clothes/[slug]/page.tsx
@@ -49,6 +49,7 @@ export async function generateStaticParams() {
 
 export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
+    console.log(slug);
 
     const res = await fetch(`https://synister-backend.onrender.com/products/slug/${slug}`, {
         next: { revalidate: 3600 }
